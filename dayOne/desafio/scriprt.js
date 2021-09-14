f2205f9e6323c488e239b2c975b97a44ff048bd3
Your Name <wallaceleonel>
2021-09-14 11:18:01 -0300

    adicionando media

---
+++ dayOne/desafio/scriprt.js
@@ -0,0 +1,11 @@
+var nome = "WallaceLeonel"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+ var notaFixada = notaFinal.toFixed(1)
+ 
+console.log("Bem-vindo " + nome)
+console.log(notaFixada)
