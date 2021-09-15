bf3ef5ae5d3acd18b4d6dfdcded190666126dcbd
Your Name <wallaceleonel>
2021-09-14 22:30:30 -0300

    adicionando desafios day 1

---
+++ dayOne/desafio/desafio1.js
@@ -0,0 +1,43 @@
+/*
+Função sendo excutada em uma unica vez 
+var nome = "WallaceLeonel"
+var nb1 = 5
+var nb2 = 9
+var nb3 = 6
+var nb4 = 4
+
+var  notaFinal=(( notaFinal=(nb1+nb2+nb3+nb4)/4),console.log("Ola :" + nome , "sua nota é :" +  notaFinal.toFixed(1))
+
+*/
+
+/*
+Forma estruturada 
+
+var nome = "WallaceLeonel"
+var nb1 = 5
+var nb2 = 9
+var nb3 = 6
+var nb4 = 4
+
+var notaFinal =notaFinal+ nb1 + nb2 + nb3 + nb4
+var resultadoFinal = notaFinal/4
+
+console.log("ola"+nome,"A sua media final foi de :"+resultadoFinal)
+
+*/
+
+/* Controle climatico 
+
+
+// controle climatico Dev_
+
+var variavel =prompt("desenha converter celcius para faraing , se sim digite 1 para Firen se não digite 2 para Kelvin ")
+
+if (variavel == 1 )var conversor=prompt("selecionado modo de conversão faring ,informe a temperatua a ser convertida :")
+faring = (1.8*conversor)+32 
+console.log("temperatura em faring é de : " + faring)
+if(variavel==2)varconversor=prompt("realizando coversão de faring para celcius ") 
+else console.log("para de zoa o programa _|_ :)")
+celcius= ((conversor-32)/1+8)
+console.log("a temperatura em celcius e de :"+celcius)
+*/
--- dayOne/desafio/scriprt.js
+++ dayOne/desafio/scriprt.js
@@ -1,11 +1,17 @@
-var nome = "WallaceLeonel"
-var notaDoPrimeiroBimestre = 9
-var notaDoSegundoBimestre = 7
-var notaDoTerceiroBimestre = 4
-var notaDoQuartoBimestre = 2
-var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
-
- var notaFixada = notaFinal.toFixed(1)
  
 console.log("Bem-vindo " + nome)
-console.log(notaFixada)
+if (notaFinal >=6)
+console.log("parabens , vc esta aprovado :) ")
+else console.log("parabens , vc foi reprovado :(")
+
+// controle climatico Dev_
+
+var variavel =prompt("desenha converter celcius para faraing , se sim digite 1 para Firen se não digite 2 para Kelvin ")
+
+if (variavel == 1 )var conversor=prompt("selecionado modo de conversão faring ,informe a temperatua a ser convertida :")
+faring = (1.8*conversor)+32 
+console.log("temperatura em faring é de : " + faring)
+if(variavel==2)varconversor=prompt("realizando coversão de faring para celcius ") 
+else console.log("para de zoa o programa _|_ :)")
+celcius= ((conversor-32)/1+8)
+console.log("a temperatura em celcius e de :"+celcius)
