0565252beca7fd65e5d14bc6130d2303af40ee13
Your Name <wallaceleonel>
2021-09-20 18:29:15 -0300

    adicionando day 4 com exercicios feitos

--- dayfour/d4.desafio/exe/d4.script.js
+++ dayfour/d4.desafio/exe/d4.script.js
@@ -1,13 +1,18 @@
-function Caminho(){
-var listaFilmes =[]
-for (var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src=" + listaFilmes[i] + ">")
-    var elementoResultado = document.getElementById("resultado");
-    var Caminho = parseInt(document.getElementById("caminhogit").value);
-    console.log(Caminho);
-    if (listaFilmes != Caminho) {
-        //add
-        
-    } // caminho existente 
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value;
+    if (filmeFavorito.endsWith("jpg")){
+    listarFilmesNaTela(filmeFavorito);
+    
+  }else{
+      console.error("Endereço não e valido");
+  }
+    document.getElementById("filme").value;
 }
+
+function listarFilmesNaTela(filme) {
+    var elementoFilmeFavorito = "img src=" +filmeFavorito + ">"
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML +elementoFilmeFavorito;
+    
 }
