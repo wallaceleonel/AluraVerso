a37f678bae3de5b45bec89d74304f670dbef3295
Your Name <wallaceleonel>
2021-09-21 15:17:09 -0300

    adicionando day 5 e ex

---
+++ dayfour/exe/d4.script.js
@@ -0,0 +1,18 @@
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value;
+    if (filmeFavorito.endsWith("jpg")){
+    listarFilmesNaTela(filmeFavorito);
+    
+  }else{
+      console.error("Endereço não e valido");
+  }
+    document.getElementById("filme").value;
+}
+
+function listarFilmesNaTela(filme) {
+    var elementoFilmeFavorito = "img src=" +filmeFavorito + ">"
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML +elementoFilmeFavorito;
+    
+}
