33ec498f5f45b861b1171f50c8ebae9b58110978
Your Name <wallaceleonel>
2021-09-22 11:38:52 -0300

    desafio day4 feito

--- dayfour/exe/d4.script.js
+++ dayfour/exe/d4.script.js
@@ -1,18 +1,9 @@
-function adicionarFilme() {
-    var filmeFavorito = document.getElementById("filme").value;
-    if (filmeFavorito.endsWith("jpg")){
-    listarFilmesNaTela(filmeFavorito);
+var listaFilmes = [  
+    "https://cdn.culturagenial.com/imagens/mv5bnzqzotk3otatndq0zi00ztvklwi0mtetmdllzjnkyznjntc4l2ltywdlxkeyxkfqcgdeqxvynju0otq0oty-v1-cke.jpg",
+    "https://scontent.fcgh53-1.fna.fbcdn.net/v/t1.6435-9/83708900_2195906644037623_3280234147938828288_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_fhT4sxZ264AX-WYR1f&_nc_ht=scontent.fcgh53-1.fna&oh=d04c97c052e63d562dacab697ccd8571&oe=61706C65",
+    "https://img.elo7.com.br/product/zoom/2679A60/big-poster-filme-matrix-revolutions-lo03-tamanho-90x60-cm-presente-geek.jpg","https://noticiasetecnologia.com/wp-content/uploads/2021/08/The-Matrix-Resurrections-2.jpg",
+    ];
     
-  }else{
-      console.error("Endereço não e valido");
-  }
-    document.getElementById("filme").value;
-}
-
-function listarFilmesNaTela(filme) {
-    var elementoFilmeFavorito = "img src=" +filmeFavorito + ">"
-    var elementoListaFilmes = document.getElementById("listaFilmes");
-    elementoListaFilmes.innerHTML =
-    elementoListaFilmes.innerHTML +elementoFilmeFavorito;
-    
-}
+    for (var i = 0; i < listaFilmes.length; i++) {
+      document.write("<img src=" + listaFilmes[i] + ">");
+    }
