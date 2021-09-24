9feab5ab6f33333b23b79c6e5a19ba7c7c442542
Your Name <wallaceleonel>
2021-09-23 22:20:20 -0300

    adicionando exercicio5

--- dayfive/d5.desafio/d5.script.js
+++ dayfive/d5.desafio/d5.script.js
@@ -1,18 +1,43 @@
+var listaFilmeNomes = [];
+var listaFilmeImagens = [];
+
 function adicionarFilme() {
-    
-    var filmeFavorito = document.getElementById("filme").value;
-    if (filmeFavorito.endsWith("jpg")){
-    listarFilmesNaTela(filmeFavorito);
-}else{
-    console.error("Endereço não e valido");
+  var filmeNome = document.getElementById("addNome").value;
+  var filmeImagem = document.getElementById("addFilme").value;
+  if (filmeImagem.endsWith(".jpg")) {
+    listaFilmeNomes.push(filmeNome);
+    listaFilmeImagens.push(filmeImagem);
+    listarFilmesNaTela(listaFilmeNomes, listaFilmeImagens);
+  } else {
+    console.error("Endereço de filme inválido");
+  }
+  document.getElementById("addNome").value = "";
+  document.getElementById("addFilme").value = "";
 }
-  document.getElementById("filme").value;
+
+//Função para remover um filme de acordo com o índice DA TELA, e não do array.
+function removerFilme() {
+  var indiceFilme = parseInt(document.getElementById("remover").value);
+  if (indiceFilme > 0) {
+    listaFilmeNomes.splice(indiceFilme - 1, 1);
+    listaFilmeImagens.splice(indiceFilme - 1, 1);
+  }
+  listarFilmesNaTela(listaFilmeNomes, listaFilmeImagens);
+  document.getElementById("remover").value = "";
 }
-    document.getElementById("filme").value;
-function listarFilmesNaTela(filme) {
-    var elementoFilmeFavorito = "img src=" +filmeFavorito + ">"
-    var elementoListaFilmes = document.getElementById("listaFilmes");
-    elementoListaFilmes.innerHTML =
-    elementoListaFilmes.innerHTML +elementoFilmeFavorito;
-    
+
+//Função para listar os filmes, percorrendo pelo array.
+function listarFilmesNaTela(listaFilmeNomes, listaFilmeImagens) {
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML = "";
+  for (var i = 0; i < listaFilmeNomes.length; i++) {
+    elementoListaFilmes.innerHTML +=
+      "<figure><img src=" +
+      listaFilmeImagens[i] +
+      "><figcaption>" +
+      (i + 1) +
+      "-" +
+      listaFilmeNomes[i] +
+      "</figcaption></figure>";
+  }
 }
