530c1d8abfa4d6d6b2722a36e4fb5411008b9751
Your Name <wallaceleonel>
2021-09-28 14:01:28 -0300

    adicionando mudanas no codigo/css/js

--- daysix/d6.desafio/d6.script.js
+++ daysix/d6.desafio/d6.script.js
@@ -1,57 +1,93 @@
-var bibig = {nome:"bibig",vitorias:1,empates:2,derrotas:1,pontos:0};
-var tigas = {nome:"bibig",vitorias:1,empates:2,derrotas:1,pontos:0};
+var jogadores = [];
 
-function calculaJogor(){
-    var pontos = jogador.vitorias * 3 + jogador.empates;
-    return pontos;
-}
-function calcularPontos(jogador){
-    var pontos = jogador.vitorias * 3 + jogador.empates;
-    return pontos;
-}
-
-bibig.pontos = calcularPontos(bibig);
-tigas.pontos = calcularPontos(tigas);
-
-var jogadores = [bibig.tigas];
-
-function exibirJogadoresNatela(jogadores){
-    var elemento = ""
-    for (var  i = 0; i < jogadores.length; i++){
-        elemento += "<tr><td>"+ jogadores[i] + "</td>"
-        elemento += "<td>"+ jogadores[i].vitorias + "</td>"
-        elemento += "<td>"+ jogadores[i].empates + "</td>"
-        elemento += "<td>"+jogadores[i].derrotas +"</td>"
-        elemento += "<td>"+jogadores[i].pontos +"</td>"
-        elemento += "<td><button onClick= 'adicionarVitoria(" + i +")'>Vitoria</button></td>"
-        elemento += "<td><button onClick= 'adicionarEmpate(" + i + ")'>Empate</button></td>"
-        elemento += "<td><button onClick= 'adicionarDerrota(" + i +")'>Derrota</button></td>"
-        elemento += "</tr>"
-
-    
-    }
-    var tabelaJogadores = 
-   document.getElementById("tabelaJogadores")
-    tabelaJogadores.innerHTML = elemento
-}
-exibirJogadoresNatela(jogadores);
-
-function adicionarVitoria(i){
-    var jogador = jogadores[i];
-    jogador.vitorias++
-    jogador.pontos = calcularPontos(jogador)
-    exibirJogadoresNatela(jogadores)
+function adicionarJogador() {
+  var nome = document.getElementById("addNome").value;
+  var foto = document.getElementById("addFoto").value;
+  if (foto.endsWith(".jpg")) {
+    jogadores.push({
+      nome: nome,
+      foto: foto,
+      vitorias: 0,
+      empates: 0,
+      derrotas: 0,
+      pontos: 0
+    });
+  } else if (foto == "") {
+    jogadores.push({
+      nome: nome,
+      foto:
+        "https://icon2.cleanpng.com/20180323/see/kisspng-mystery-royalty-free-clip-art-mysterious-man-cliparts-5ab49f5896b3c7.1616473215217867126173.jpg",
+      vitorias: 0,
+      empates: 0,
+      derrotas: 0,
+      pontos: 0
+    });
+  } else {
+    console.error("Insira um endereço válido.");
+  }
+  document.getElementById("addNome").value = "";
+  document.getElementById("addFoto").value = "";
+  exibeJogadoresNaTela(jogadores);
 }
 
-function adicionarEmpate(i){
-    var jogador =jogadores [i];
-    jogador.empates++;
-    jogador.pontos = calcularPontos(jogador);
-    exibirJogadoresNatela(jogadores);
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
 }
 
-function adicionarDerrota(i){
-    var jogador =jogadores[i];
-    jogador.derrotas++;
-    exibirJogadoresNatela(jogadores);
+function exibeJogadoresNaTela(jogadores) {
+  var elemento = "";
+  for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td><img src=" + jogadores[i].foto + "></td>";
+    elemento += "<td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarEmpate(i) {
+  var jogador = jogadores[i];
+  jogador.empates++;
+  jogador.pontos = calculaPontos(jogador);
+  exibeJogadoresNaTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++;
+  exibeJogadoresNaTela(jogadores);
+}
+
+function resetarPontos() {
+  for (var i = 0; i < jogadores.length; i++) {
+    jogadores[i].vitorias = 0;
+    jogadores[i].empates = 0;
+    jogadores[i].derrotas = 0;
+    jogadores[i].pontos = calculaPontos(jogadores[i]);
+  }
+  exibeJogadoresNaTela(jogadores);
+}
+
+function resetarTabela() {
+  jogadores = [];
+  exibeJogadoresNaTela(jogadores);
 }
